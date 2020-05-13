0f85e7e242e28924a4417e5f0b8c453cb0bcc9b1
Your Name <you@example.com>
2020-05-12 21:32:48 -0700

    lab7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+  }
+
+// success, info, warning, danger
+  showToast() {
+
+    const rand = Math.floor(Math.random() * 4);
+    const toasttype = this.toastTypes[rand];
+    const message = 'Hi! This is a message. My random number is ' + rand + '!';
+    const duration = 4000;
+    this.toastService.showToast(toasttype, message, duration);
+  }
 }
